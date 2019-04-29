bd3776e564522acb7252c1cf48590ca38b1085af
Lukasz Kurek <lukascoorek@gmail.com>
2019-04-29 15:34:59 +0200

    add watchAll to test in json

--- fibo.js
+++ fibo.js
@@ -1,19 +1,4 @@
-// const fib = (index, cache = []) => {
-//   if (isNaN(index)) return false;
-//   if (cache[index]) return cache[index];
-//   else {
-//     if (index < 3) return 1;
-//     else {
-//       console.log(cache);
-
-//       return (cache[index] = fib(index - 1, cache) + fib(index - 2, cache));
-//     }
-//   }
-// };
-
-// console.log(fib(5));
-
-function fib(n) {
+const fib = n => {
   if (isNaN(n)) return false;
   if (n === 0) return [];
   if (n === 1) return [0];
@@ -24,7 +9,5 @@ function fib(n) {
     for (let i = 2; i < n; i++) fibb.push(fibb[i - 1] + fibb[i - 2]);
     return fibb;
   }
-}
-console.log(fib(12));
-
+};
 module.exports = fib;
--- fibo.test.js
+++ fibo.test.js
@@ -1,17 +1,18 @@
 const fib = require("./fibo");
 
-test("NaN to be false", () => {
+test("Return false for NaN parameter", () => {
   expect(fib(NaN)).toBeFalsy();
 });
-test("fib function to be defined", () => {
-  expect(fib()).toBeDefined();
-});
-test("Return [] for 0", () => {
+test("Return [] for fib(0)", () => {
   expect(fib(0)).toEqual([]);
 });
-test("Return [0] for 1", () => {
+test("Return [0] for fib(1)", () => {
   expect(fib(1)).toEqual([0]);
 });
+test("Return [0,1,1,2,3,5,8] for fib(7)", () => {
+  expect(fib(7)).toEqual([0, 1, 1, 2, 3, 5, 8]);
+});
 test("Return 12 for fib(12).length", () => {
-  expect(fib(12).length).toBe(12);
+  const result = fib(12);
+  expect(result.length).toBe(12);
 });
